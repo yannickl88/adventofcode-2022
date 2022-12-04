3a6b18571bb73ff40ea4289866e2b48da80298ba
Yannick de Lange <yannick.l.88@gmail.com>
2022-12-04 16:53:15 +0100

    Day 4 part 1

---
+++ src/nl/yannickl88/adventofcode/days/Day4.java
@@ -0,0 +1,68 @@
+package nl.yannickl88.adventofcode.days;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day4 {
+
+    static class Section {
+        public int start;
+        public int end;
+
+        Section(String range) {
+            String[] parts = range.split("-");
+
+            start = Integer.parseInt(parts[0]);
+            end = Integer.parseInt(parts[1]);
+        }
+
+        public boolean fullyOverlaps(Section other) {
+            return start <= other.start && end >= other.end;
+        }
+
+        public void print(int size) {
+            StringBuilder row = new StringBuilder();
+
+            for (int i = 1; i <= size; i++) {
+                if (i >= start && i <= end) {
+                    row.append(i);
+                } else {
+                    row.append(".");
+                }
+            }
+
+            row.append("  ");
+            row.append(start);
+            row.append("-");
+            row.append(end);
+
+            System.out.println(row);
+        }
+    }
+
+    public Day4() {
+        File input = new File("inputs/day4/input.txt");
+        Scanner pairs;
+        try {
+            pairs = new Scanner(input);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        int overlappingSections = 0;
+
+        while (pairs.hasNextLine()) {
+            String[] pair = pairs.nextLine().split(",");
+
+            Section firstSection = new Section(pair[0]);
+            Section secondSection = new Section(pair[1]);
+
+            if (firstSection.fullyOverlaps(secondSection) || secondSection.fullyOverlaps(firstSection)) {
+                overlappingSections++;
+            }
+        }
+
+        System.out.printf("%d pairs fully contain the other.\n", overlappingSections);
+    }
+}

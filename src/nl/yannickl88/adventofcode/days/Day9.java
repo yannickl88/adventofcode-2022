61a11a1ffea82307dccecb687c62e20b896e2d86
Yannick de Lange <yannick.l.88@gmail.com>
2022-12-09 19:54:09 +0100

    Day 9 part 2

--- src/nl/yannickl88/adventofcode/days/Day9.java
+++ src/nl/yannickl88/adventofcode/days/Day9.java
@@ -12,8 +12,7 @@ public class Day9 {
         R,U,D,L
     }
     static class Position {
-        private final int x;
-        private final int y;
+        private int x, y;
 
         Position(int x, int y) {
             this.x = x;
@@ -21,12 +20,18 @@ public class Day9 {
         }
 
         public Position move(Direction direction) {
-            return switch (direction) {
-                case R -> new Position(x + 1, y);
-                case U -> new Position(x, y + 1);
-                case D -> new Position(x, y - 1);
-                case L -> new Position(x - 1, y);
-            };
+            switch (direction) {
+                case R -> x += 1;
+                case U -> y += 1;
+                case D -> y -= 1;
+                case L -> x -= 1;
+            }
+
+            return this;
+        }
+
+        Position copy() {
+            return new Position(x, y);
         }
 
         @Override
@@ -45,62 +50,105 @@ public class Day9 {
         }
     }
     static class Rope {
-        Position head, tail;
+        Position[] knots;
+
+        int minX = 0, minY = 0, maxX = 0, maxY = 0;
         final ArrayList<Position> steps;
 
         public Rope() {
-            head = new Position(0, 0);
-            tail = new Position(0, 0);
+            knots = new Position[]{
+                    new Position(0, 0), // H
+                    new Position(0, 0), // 1
+                    new Position(0, 0), // 2
+                    new Position(0, 0), // 3
+                    new Position(0, 0), // 4
+                    new Position(0, 0), // 5
+                    new Position(0, 0), // 6
+                    new Position(0, 0), // 7
+                    new Position(0, 0), // 8
+                    new Position(0, 0), // 9, tail
+            };
             steps = new ArrayList<>();
-            steps.add(tail);
         }
 
         void move(Direction direction, int steps) {
             System.out.printf("== %s %d ==\n", direction.name(), steps);
 
-            for (int i = 0; i < steps; i++) {
-                simulateMovementStep(direction);
-                this.steps.add(tail);
+            for (int s = 0; s < steps; s++) {
+                knots[0].move(direction);
+                for (int i = 0; i < 9; i++) {
+                    simulateMovementStep(knots[i], knots[i + 1]);
+                }
+//                print();
+                this.steps.add(knots[9].copy());
             }
         }
-        private void simulateMovementStep(Direction direction) {
-            head = head.move(direction);
+        private void simulateMovementStep(Position head, Position tail) {
             int deltaX = tail.x - head.x;
             int deltaY = tail.y - head.y;
 
             if (Math.abs(deltaX) + Math.abs(deltaY) > 2) {
                 if (deltaX < 0) {
                     if (deltaY < 0) {
-                        tail = tail.move(Direction.U).move(Direction.R);
+                        tail.move(Direction.U).move(Direction.R);
                     } else if (deltaY > 0) {
-                        tail = tail.move(Direction.D).move(Direction.R);
+                        tail.move(Direction.D).move(Direction.R);
                     }
                 } else if (deltaX > 0) {
                     if (deltaY < 0) {
-                        tail = tail.move(Direction.U).move(Direction.L);
+                        tail.move(Direction.U).move(Direction.L);
                     } else if (deltaY > 0) {
-                        tail = tail.move(Direction.D).move(Direction.L);
+                        tail.move(Direction.D).move(Direction.L);
                     }
                 }
             } else {
                 if (deltaX < -1) {
-                    tail = tail.move(Direction.R);
+                    tail.move(Direction.R);
                 } else if (deltaX > 1) {
-                    tail = tail.move(Direction.L);
+                    tail.move(Direction.L);
                 }
 
                 if (deltaY < -1) {
-                    tail = tail.move(Direction.U);
+                    tail.move(Direction.U);
                 } else if (deltaY > 1) {
-                    tail = tail.move(Direction.D);
+                    tail.move(Direction.D);
                 }
             }
+
+            minX = Math.min(minX, head.x);
+            minX = Math.min(minX, tail.x);
+            maxX = Math.max(maxX, head.x);
+            maxX = Math.max(maxX, tail.x);
+
+            minY = Math.min(minY, head.y);
+            minY = Math.min(minY, tail.y);
+            maxY = Math.max(maxY, head.y);
+            maxY = Math.max(maxY, tail.y);
 
 //            System.out.printf("head is at (%d, %d), tail at (%d, %d)\n", head.x, head.y, tail.x, tail.y);
         }
 
         public Set<Position> getTailPosition() {
             return new HashSet<>(this.steps);
+        }
+
+        public void print() {
+            System.out.println();
+
+            for (int y = maxY; y >= minY; y--) {
+                for (int x = minX; x <= maxX; x++) {
+                    String symbol = ".";
+                    for (int k = 0; k < 10; k++) {
+                        if(knots[k].equals(new Position(x, y))) {
+                            symbol = k == 0 ? "H" : String.valueOf(k);
+                            break;
+                        }
+                    }
+
+                    System.out.printf(symbol);
+                }
+                System.out.println();
+            }
         }
     }
 
@@ -114,6 +162,7 @@ public class Day9 {
         }
 
         Rope rope = new Rope();
+        rope.print();
 
         while (moves.hasNext()) {
             String[] instruction = moves.nextLine().split(" ");

08c938634c403af501342996c7bb9cd907b075c0
Yannick de Lange <yannick.l.88@gmail.com>
2022-12-11 22:50:31 +0100

    Day 11 part 2

--- src/nl/yannickl88/adventofcode/days/Day11.java
+++ src/nl/yannickl88/adventofcode/days/Day11.java
@@ -9,74 +9,27 @@ import java.util.stream.Collectors;
 
 public class Day11 {
     static class Item {
-        private static final int[] factors = new int[]{
-                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999
-        };
-        private int[] exponents;
+        private static final int modulo = 9699690;
+        private long value;
 
         Item(int number) {
-            exponents = factorize(number);
+            value = number;
         }
 
-        static int[] factorize(long number) {
-            int[] exponents = new int[factors.length];
-
-            while (number > 1) {
-                boolean hasFactored = false;
-                for (int i = 0; i < factors.length; i++) {
-                    if (number % factors[i] == 0) {
-                        number = number / factors[i];
-                        exponents[i]++;
-
-                        hasFactored = true;
-                        break;
-                    }
-                }
-
-                if (!hasFactored) {
-                    throw new RuntimeException("Cannot factor number " + number);
-                }
-            }
-
-            return exponents;
+        void add(long value) {
+            this.value = this.value % modulo + value % modulo;
         }
 
-        Item add(int value) {
-            this.exponents = factorize(this.value() + (long) value);
-
-            return this;
+        void multiply(long value) {
+            this.value = this.value % modulo * value % modulo;
         }
 
-        Item multiply(int value) {
-            int[] exponents = factorize(value);
-
-            for (int i = 0; i < factors.length; i++) {
-                this.exponents[i] += exponents[i];
-            }
-
-            return this;
-        }
-
-        Item square() {
-            for (int i = 0; i < factors.length; i++) {
-                this.exponents[i] += exponents[i];
-            }
-
-            return this;
+        void square() {
+            this.multiply(this.value);
         }
 
         boolean isDivisibleBy(int value) {
-            return false;
-        }
-
-        long value() {
-            long total = 1;
-
-            for (int i = 0; i < factors.length; i++) {
-                total *= (long) Math.pow(factors[i], exponents[i]);
-            }
-
-            return total;
+            return this.value % value == 0;
         }
     }
 
@@ -109,7 +62,7 @@ public class Day11 {
     }
 
     public Day11() {
-        File input = new File("inputs/day11/test.txt");
+        File input = new File("inputs/day11/input.txt");
         Scanner configuration;
         try {
             configuration = new Scanner(input);

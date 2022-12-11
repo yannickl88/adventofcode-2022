849c048143370af7cffe6b2d02402be01cfde490
Yannick de Lange <yannick.l.88@gmail.com>
2022-12-11 17:04:28 +0100

    Day 11 part 2

--- src/nl/yannickl88/adventofcode/days/Day11.java
+++ src/nl/yannickl88/adventofcode/days/Day11.java
@@ -3,26 +3,91 @@ package nl.yannickl88.adventofcode.days;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Day11 {
     static class Item {
-        long worryLevel;
+        private static final int[] factors = new int[]{
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999
+        };
+        private int[] exponents;
 
-        public Item(int level) {
-            worryLevel = level;
+        Item(int number) {
+            exponents = factorize(number);
+        }
+
+        static int[] factorize(long number) {
+            int[] exponents = new int[factors.length];
+
+            while (number > 1) {
+                boolean hasFactored = false;
+                for (int i = 0; i < factors.length; i++) {
+                    if (number % factors[i] == 0) {
+                        number = number / factors[i];
+                        exponents[i]++;
+
+                        hasFactored = true;
+                        break;
+                    }
+                }
+
+                if (!hasFactored) {
+                    throw new RuntimeException("Cannot factor number " + number);
+                }
+            }
+
+            return exponents;
+        }
+
+        Item add(int value) {
+            this.exponents = factorize(this.value() + (long) value);
+
+            return this;
+        }
+
+        Item multiply(int value) {
+            int[] exponents = factorize(value);
+
+            for (int i = 0; i < factors.length; i++) {
+                this.exponents[i] += exponents[i];
+            }
+
+            return this;
+        }
+
+        Item square() {
+            for (int i = 0; i < factors.length; i++) {
+                this.exponents[i] += exponents[i];
+            }
+
+            return this;
+        }
+
+        boolean isDivisibleBy(int value) {
+            return false;
+        }
+
+        long value() {
+            long total = 1;
+
+            for (int i = 0; i < factors.length; i++) {
+                total *= (long) Math.pow(factors[i], exponents[i]);
+            }
+
+            return total;
         }
     }
 
     static class Monkey {
         private int name;
-        private int inspections = 0;
+        private long inspections = 0;
         private final ArrayList<Item> items = new ArrayList<>();
-        private final Function<Long, Long> operation;
-        private final Function<Long, Monkey> test;
+        private final Consumer<Item> operation;
+        private final Function<Item, Monkey> test;
 
-        public Monkey(int name, List<Item> starting, Function<Long, Long> operation, Function<Long, Monkey> test) {
+        public Monkey(int name, List<Item> starting, Consumer<Item> operation, Function<Item, Monkey> test) {
             this.name = name;
             this.operation = operation;
             this.test = test;
@@ -37,26 +102,14 @@ public class Day11 {
 
                 // Inspects
                 inspections++;
-                if (verbose) System.out.printf("  Monkey inspects an item with a worry level of %d.\n", item.worryLevel);
-
-                // Apply operation
-                item.worryLevel = this.operation.apply(item.worryLevel);
-                if (verbose) System.out.printf("    Worry level is updated to %d.\n", item.worryLevel);
-
-                // Boredom
-                item.worryLevel = item.worryLevel / 3;
-                if (verbose) System.out.printf("    Monkey gets bored with item. Worry level is divided by 3 to %d.\n", item.worryLevel);
-
-                // Select
-                Monkey target = this.test.apply(item.worryLevel);
-                target.items.add(item);
-                if (verbose) System.out.printf("    Item with worry level %d is thrown to monkey %d.\n", item.worryLevel, target.name);
+                this.operation.accept(item);
+                this.test.apply(item).items.add(item);
             }
         }
     }
 
     public Day11() {
-        File input = new File("inputs/day11/input.txt");
+        File input = new File("inputs/day11/test.txt");
         Scanner configuration;
         try {
             configuration = new Scanner(input);
@@ -69,15 +122,15 @@ public class Day11 {
         while (configuration.hasNext()) {
             int name = Integer.parseInt(configuration.nextLine().substring(7, 8));
             List<Item> starting = Arrays.stream(
-                    configuration
-                            .nextLine()
-                            .substring(18)
-                            .split(",")
+                            configuration
+                                    .nextLine()
+                                    .substring(18)
+                                    .split(",")
                     )
                     .map(s -> new Item(Integer.parseInt(s.trim())))
                     .collect(Collectors.toList());
-            Function<Long, Long> operation = parseOperations(configuration.nextLine().substring(19));
-            Function<Long, Monkey> test = parseCondition(
+            Consumer<Item> operation = parseOperations(configuration.nextLine().substring(19));
+            Function<Item, Monkey> test = parseCondition(
                     Integer.parseInt(configuration.nextLine().substring(21)),
                     Integer.parseInt(configuration.nextLine().substring(29)),
                     Integer.parseInt(configuration.nextLine().substring(30)),
@@ -90,47 +143,43 @@ public class Day11 {
                 configuration.nextLine(); // empty line
         }
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10000; i++) {
             for (Monkey monkey : monkeys) {
-                monkey.play(true);
+                monkey.play(false);
             }
 
-            System.out.printf("After round %d, the monkeys are holding items with these worry levels:\n", i + 1);
+            System.out.printf("== After round %d ==\n", i + 1);
             for (Monkey monkey : monkeys) {
-                System.out.printf("  Monkey %d: %s\n", monkey.name, monkey.items.stream().map(item -> String.valueOf(item.worryLevel)).collect(Collectors.joining(", ")));
+                System.out.printf("Monkey %d inspected items %d times.\n", monkey.name, monkey.inspections);
             }
+            System.out.println();
         }
 
-        System.out.println();
 
-        for (Monkey monkey : monkeys) {
-            System.out.printf("Monkey %d inspected items %d times.\n", monkey.name, monkey.inspections);
-        }
-
-        PriorityQueue<Monkey> monkeyInspections = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.inspections, o1.inspections));
+        PriorityQueue<Monkey> monkeyInspections = new PriorityQueue<>((o1, o2) -> Long.compare(o2.inspections, o1.inspections));
         monkeyInspections.addAll(monkeys);
 
-        int monkeyBusiness = Objects.requireNonNull(monkeyInspections.poll()).inspections * Objects.requireNonNull(monkeyInspections.poll()).inspections;
+        long monkeyBusiness = Objects.requireNonNull(monkeyInspections.poll()).inspections * Objects.requireNonNull(monkeyInspections.poll()).inspections;
 
         System.out.printf("the level of monkey business is %d after 20 rounds of stuff-slinging simian shenanigans\n", monkeyBusiness);
     }
 
-    private Function<Long, Long> parseOperations(String operation) {
+    private Consumer<Item> parseOperations(String operation) {
         if (operation.matches("^old \\* old$")) {
-            return (Long old) -> old * old;
+            return item -> item.square();
         }
         if (operation.matches("^old \\* [0-9]+$")) {
-            long constant = Integer.parseInt(operation.substring(6));
-            return (Long old) -> old * constant;
+            int constant = Integer.parseInt(operation.substring(6));
+            return item -> item.multiply(constant);
         }
         if (operation.matches("^old \\+ [0-9]+$")) {
-            long constant = Integer.parseInt(operation.substring(6));
-            return (Long old) -> old + constant;
+            int constant = Integer.parseInt(operation.substring(6));
+            return item -> item.add(constant);
         }
         throw new RuntimeException("Unknown operation " + operation);
     }
 
-    private Function<Long, Monkey> parseCondition(long test, int ifTrue, int ifFalse, ArrayList<Monkey> monkeys) {
-        return (Long old) -> monkeys.get(old % test == 0 ? ifTrue : ifFalse);
+    private Function<Item, Monkey> parseCondition(int test, int ifTrue, int ifFalse, ArrayList<Monkey> monkeys) {
+        return (Item item) -> monkeys.get(item.isDivisibleBy(test) ? ifTrue : ifFalse);
     }
 }
